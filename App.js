e234ccfdb4118fc7cdf38a2d180748e772ee31e8
Christopher Linares <cbluiux@gmail.com>
2021-08-05 14:29:36 -0600

    created src/screens/SearchScreen

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Navigator = createStackNavigator(
+  { Search: SearchScreen },
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: { title: 'Business Search' },
+  }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
+
+export default createAppContainer(Navigator);
